20d0fd9720a6855a7484741885fbac2bc23a0f49
Jack Anderson <whotfisjax@gmail.com>
2024-05-22 06:03:30 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { MenuList, MenuListItem, styleReset } from 'react95';
 import original from 'react95/dist/themes/original';
 import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
 import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-import TG from './TG.png';
+import TG from './tg.png';
 import Xlogo from './Xlogo.png';
 
 const GlobalStyles = createGlobalStyle`
